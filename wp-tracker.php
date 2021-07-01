528490628b17f9e54b29e18d1d7a4e33734a8837
Anthony ARJONA <mimso@outlook.fr>
2021-07-01 12:14:12 +0200

    Ajout verification version php + interdire acces direct

--- wp-tracker.php
+++ wp-tracker.php
@@ -10,3 +10,21 @@
  * Text Domain: wp-tracker
  */
 
+define( 'TRACKER_VERSION', '1.0' );
+define( 'REQUIRED_PHP_VERSION', '7.2' );
+
+//block direct access to ext
+if(!defined( 'ABSPATH')) {
+    exit;
+}
+
+//add message in admin panel if php version is invalid
+if (!version_compare(phpversion(), REQUIRED_PHP_VERSION, "<=")) {
+    if ( is_admin() ) {
+        function wp_tracker_php_notice() {
+            printf( '<div class="notice notice-error"><p>WP Tracker plugin requires at least PHP version ' . REQUIRED_PHP_VERSION . ', but installed is version ' . PHP_VERSION . '.</p></div>');
+        }
+        add_action('admin_notices', 'wp_tracker_php_notice');
+    }
+    return;
+}
