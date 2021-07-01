0d2d9e46ad5fd4c5f4b7b8ab17089b438ed8c735
NilsMillot <compte.mxs@hotmail.fr>
2021-07-01 11:15:01 +0200

    boiler plate

---
+++ wp-tracker.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: WP Tracker
+ * Plugin URI: https://github.com/Mimso/wp-tracker
+ * Description: Detailed web tracker of your website
+ * Version: 1.0
+ * Author: Renoux Oceane, Arjona Anthony, Millot Nils
+ * Author URI: https://github.com/Mimso
+ * License: ''
+ * Text Domain: wp-tracker
+ */
+
