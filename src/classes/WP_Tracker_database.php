6f1dd99b9266df6c9d0a75a00199be5bd7f9d6a7
Anthony ARJONA <mimso@outlook.fr>
2021-07-19 10:50:26 +0200

    [UPDATE] Database method
    Création automatique des tables à partir d'un tableau

--- src/classes/WP_Tracker_database.php
+++ src/classes/WP_Tracker_database.php
@@ -10,12 +10,27 @@ class WP_Tracker_database
 
     private $database;
     private $tables = [];
+    private $db_prefix;
+    private $query;
 
     public function __construct() {
         global $wpdb;
         $this->database = $wpdb;
+        $this->db_prefix = $wpdb->prefix;
         $this->tables    =  [
-            'tracker' => $this->database->prefix . 'wordpress_tracker'
+            'wordpress_tracker' => [
+                'name' => [
+                    'type' => 'varchar',
+                    'size' => 255,
+                ],
+                'description' => [
+                    'type' => 'varchar',
+                    'size' => 255,
+                ],
+                'value' => [
+                    'type' => 'longtext',
+                ]
+            ]
         ];
     }
 
@@ -39,14 +54,36 @@ class WP_Tracker_database
     }
 
     protected function createTableIfNotExist() {
-        $sql = "CREATE TABLE IF NOT EXISTS {$this->tables['tracker']} (
-                      id int(5) NOT NULL AUTO_INCREMENT,
-                      visit int(5) DEFAULT NULL,
-                      PRIMARY KEY (id)
-            ) {$this->getCharsetCollate()};";
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-        dbDelta($sql);
+        dbDelta($this->queryBuilder());
     }
 
+    protected function queryBuilder() {
+        foreach ($this->tables as $table_name => $table_columns) {
+            $column_exclude = ['isActive', 'isDeleted', 'createAt', 'updateAt'];
+            $index_authorized = ['PRIMARY KEY', 'UNIQUE', 'INDEX', 'FULLTEXT', 'SPATIAL'];
+
+            $this->query .= 'CREATE TABLE IF NOT EXISTS `' . $this->db_prefix . $table_name . '` (';
+            $this->query .= '`id` int(11) NOT NULL AUTO_INCREMENT, PRIMARY KEY(id),';
+
+            foreach ($table_columns as $column_name => $column_params) {
+                if (!in_array($column_name, $column_exclude)) {
+                    $this->query .= '`' . $column_name . '` ';
+                    $this->query .= $column_params['type'];
+                    $this->query .= isset($column_params['size']) ? '(' . $column_params['size'] . ')' : '';
+                    $this->query .= isset($column_params['null_permitted']) ? '' : ' NOT NULL';
+                    $this->query .= isset($column_params['default_value']) ? ' DEFAULT ' . (is_string($column_params['default_value']) ? ($column_params['default_value'] != 'null' ? '\'' : '') . strtoupper($column_params['default_value']) . ($column_params['default_value'] != 'null' ? '\'' : '') : $column_params['default_value']) : '';
+                    $this->query .= (isset($column_params['auto_increment']) && $column_params['auto_increment'] != false) ? ' AUTO_INCREMENT ' : '';
+                    $this->query .= (isset($column_params['index']) && !in_array($column_params['index'], $index_authorized)) ? $column_params['index'] : '';
+                    $this->query .= isset($column_params['comment']) ? ' COMMENT \'' . $column_params['comment'] . '\'' : '';
+                    $this->query .= ', ';
+                }
+            }
+            $this->query .= ' `createAt` DATETIME DEFAULT CURRENT_TIMESTAMP,';
+            $this->query .= ' `updateAt` DATETIME on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP';
+            $this->query .= ') ' . $this->getCharsetCollate();
+        }
+        return $this->query;
+    }
 
 }
