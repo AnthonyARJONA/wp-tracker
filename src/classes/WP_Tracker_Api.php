0ff1b8fe9737b82e4e1acd30684e14a9faf4f4dd
Anthony ARJONA <mimso@outlook.fr>
2021-07-23 12:13:36 +0200

    [ADD] APi

--- src/classes/WP_Tracker_Api.php
+++ src/classes/WP_Tracker_Api.php
@@ -9,15 +9,19 @@ class WP_Tracker_Api
 {
 
     const IPINFO_URI = 'https://ipinfo.io/';
-    const IPINFO_KEY = '';
+    const IPINFO_KEY = '0165528f5868fd';
 
+    /**
+     * return api data in array
+     * @return array|string
+     */
     public static function getIpInfo() {
         try {
             //IPInfo free 50000 rate limit per month
                 $curl = new WP_Tracker_Curl(self::IPINFO_URI . WP_Tracker_Client::getClientIp(), [
                 CURLOPT_HTTPHEADER => ['Authorization: Bearer ' . self::IPINFO_KEY]
             ]);
-            return $curl->exec();
+            return json_decode($curl->exec(), true);
         } catch (\Exception $exception) {
             return $exception->getMessage();
         }
