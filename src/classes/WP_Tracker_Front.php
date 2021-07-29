f03cab8128fc28d617b29040b11231c41a1c7802
Océane <oceane.renoux@hotmail.fr>
2021-07-29 02:01:14 +0200

    [ADD] Front + donnée stat site

--- src/classes/WP_Tracker_Front.php
+++ src/classes/WP_Tracker_Front.php
@@ -20,6 +20,63 @@ class WP_Tracker_Front {
         var_dump($db->insertVisitor());
         var_dump($db->getVisitedPageThisMonth());
         echo '</pre>';
+        echo '<h1>Statistique du site</h1>';
+        echo '<div class="col-xs-6 col-sm-6 col-md-6 col-lg-4" style="padding:0;">
+			<div class="wpvf_info wpvf_info_blue wpvf_info_mobile">
+				<span class="wpvf_info_title_mobile">'.$db->getTodayVisit() .'</span><br>
+			       <br>
+				<br>
+				Nombre de visite Aujourd\'hui<br>
+			</div>
+		</div>';
+        echo '<div class="col-xs-6 col-sm-6 col-md-6 col-lg-4" style="padding:0;">
+			<div class="wpvf_info wpvf_info_pink">
+				<span class="wpvf_info_title_mobile">'.$db->getVisitLastSevenDays() .'</span><br>
+			       <br>
+				<br>
+				Nombre de visite sur 7 jour<br>
+			</div>
+		</div>';
+        echo '<div class="col-xs-6 col-sm-6 col-md-6 col-lg-4" style="padding:0;">
+			<div class="wpvf_info wpvf_info_red">
+				<span class="wpvf_info_title_mobile">'.$db->getUniqueVisitThisMonth() .'</span><br>
+			       <br>
+				<br>
+				Nombre de visite sur un mois<br>
+			</div>
+		</div>';
+        echo '<div class="col-xs-6 col-sm-6 col-md-6 col-lg-4" style="padding:0;">
+			<div class="wpvf_info wpvf_info_darkblue">
+				<span class="wpvf_info_title_mobile">'.$db->getTodayVisitedPage() .'</span><br>
+			       <br>
+				<br>
+				Nombre de page vue aujourd\'hui<br>
+			</div>
+		</div>';
+        echo '<div class="col-xs-6 col-sm-6 col-md-6 col-lg-4" style="padding:0;">
+			<div class="wpvf_info wpvf_info_darkpink">
+				<span class="wpvf_info_title_mobile">'.$db->getVisitedPageLastSevenDays() .'</span><br>
+			       <br>
+				<br>
+				Nombre de page vue sur 7 jour<br>
+			</div>
+		</div>';
+        echo '<div class="col-xs-6 col-sm-6 col-md-6 col-lg-4" style="padding:0;">
+			<div class="wpvf_info wpvf_info_darkred">
+				<span class="wpvf_info_title_mobile">'.$db->getVisitedPageThisMonth() .'</span><br>
+			       <br>
+				<br>
+				Nombre de page vue sur un mois<br>
+			</div>
+		</div>';
+        echo  '<div class="col-xs-6 col-sm-6 col-md-6 col-lg-3" style="padding:0;">
+			   <div class="wpvf_info wpvf_info_green">
+				<span class="wpvf_info_title_mobile"> '. $db->getMaxVisit() .'</span><br>
+                 <br>
+                Nombre record de visite sur le site
+                 </div>
+                </div>';
+
     }
 
 }
